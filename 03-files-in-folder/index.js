7c72e07ebb082ff7e34d49217c1aab9a8d664127
IuliiaPi <julia.pidlisna@gmail.com>
2022-05-24 19:12:36 -0400

    HTML-builder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,25 +1,23 @@
 const fs = require('fs');
 const path = require('path');
-
-
 let filePath = path.join(__dirname, './secret-folder');
-
-fs.readdir(filePath, (err, files) => {
-    
-  if (err)
+  
+fs.readdir(filePath, {withFileTypes: true}, (err, files) => {  
+  if (err) {
     console.log(err);
-  else {
-    
+  } else {
     files.forEach(file => {
-      if ((path.extname(file) == '.txt') || (path.extname(file) == '.js') || (path.extname(file) == '.css') || (path.extname(file) == '.csv'))
-        fs.stat(filePath, (err, stats) => {
+      if (file.isFile()) {
+        let filePathItem = filePath + '/' + file['name'];
+        fs.stat(filePathItem, (err, stats) => {
           if (err) {
             console.error(err);
             return;
           }
-          console.log((path.basename(file, path.extname(file))) + ' - ' + ((path.extname(file)).slice(1)) + ' - ' + (stats.size));
+          console.log((path.basename(filePathItem, path.extname(filePathItem))) + ' - ' + ((path.extname(filePathItem)).slice(1)) + ' - ' + (stats.size));
         });
-        
+      } 
     });
   }
 });
+  
