1506b609686336a7930bc0076207301fdc7af988
IuliiaPi <julia.pidlisna@gmail.com>
2022-05-24 20:08:04 -0400

    HTML-builder

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const { stdout } = require('process');
-const input = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
-input.on('data', data => stdout.write(data));
+let filePath = path.join(__dirname, './text.txt');
+
+const stream = fs.createReadStream(filePath, 'utf8');
+stream.on('data', data => stdout.write(data));
